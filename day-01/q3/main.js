2d4f5340810588cf3b0ffe370ecd41ad59a594bd
Muntaha Noor <muntahanoor648@gmail.com>
2024-03-18 17:58:15 +0500

    Initial commit

---
+++ day-01/q3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myName = "Muntaha Noor";
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
---
+++ day-01/q3/main.ts
@@ -0,0 +1,7 @@
+let myName: string = "Muntaha Noor";
+
+console.log(myName.toLowerCase())
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
+
+export {}
